e2342fda579ec303af8be966bdc98cd131199ee3
Jon Herman <jherman@localhost.localdomain>
2022-07-14 15:48:35 -0400

    fixing math

--- temperature.py
+++ temperature.py
@@ -1,6 +1,6 @@
-fahrenheit = input("What is the temperature in Fahrenheit? ")
-celsius = int(fahrenheit) - 32 * .5556
-kelvin = int(celsius) + 273.15
+fahrenheit = int(input("What is the temperature in Fahrenheit? "))
+celsius = (fahrenheit - 32) * .5556
+kelvin = celsius + 273.15
 Cfloat = "{:.2f}".format(celsius)
 Kfloat = "{:.2f}".format(kelvin)
 print("The temperature in Celsius is " + Cfloat + " and the temperature in Kelvin is " + Kfloat)
